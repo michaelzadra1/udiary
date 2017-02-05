e83eed2a1fa5595dff222358606514983207fdbe
Kal Radikov <Kal Radikov>
2017-02-04 21:03:29 -0500

    setting.py and analysis class updated

--- udiary/analysis/models.py
+++ udiary/analysis/models.py
@@ -1,18 +1,21 @@
 from django.db import models
 from entries.models import Entry
+from django.contrib.postgres.operations import HStoreExtension
+
 # Create your models here.
 class Analysis(models.Model):
-	entry = models.ForeignKey(Entry, on_delete = models.CASCADE, related_name='parent_entry')
-	updated_at = models.DateTimeField(auto_now=True)
-	sentiment = models.TextField(default='');
-	texttags = models.TextField(default='');
-	keywords = models.TextField(default='');
-	people = models.TextField(default='');
-	places = models.TextField(default='');
-	personality = models.TextField(default='');
-	emotion = models.TextField(default='');
-	personas = models.TextField(default='');
+	entry = models.ForeignKey(Entry, on_delete = models.CASCADE, related_name='parent_entry') #can stay
+	updated_at = models.DateTimeField(auto_now=True) #can stay
+	sentiment = models.FloatField(default='');
+	texttags = models.HStoreField(default='');
+	keywords = models.HStoreField(default='');
+	people = models.ArrayField(models.CharField());
+	places = models.ArrayField(models.CharField());
+	personality = models.HStoreField(default='');
+	emotion = models.HStoreField(default='');
+	personas = models.HStoreField(default='');
 	
+'''	
 	sentiment_tags = models.TextField(default='');
 	sentiment_keywords = models.TextField(default='');
 	sentiment_people = models.TextField(default='');
@@ -23,6 +26,7 @@ class Analysis(models.Model):
 	emotion_places = models.TextField(default='');
 	def __init__(self, text):
 		self.analyze()
+		'''
 		
 	def __str__(self):
 		return self.id + " " + self.entry;
